aac764817162f5c589478cb88ff5d45ffa83214e
TeKronS <asper.tk93@gmail.com>
2024-11-10 11:03:30 -0400

    modified location

--- src/app-sections/Footer.tsx
+++ src/app-sections/Footer.tsx
@@ -42,12 +42,12 @@ export const FooterSection = () => {
       <section className={styles.ubication}>
         <h4>Ubícanos</h4>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d487.18218293943164!2d-68.57814752178483!3d9.654708670096129!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e7e142a983d9ead%3A0xbacfec7336c88a51!2sEzequiel%20Zamora%20Airport!5e1!3m2!1sen!2sve!4v1644282260453!5m2!1sen!2sve"
+          src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d222.45987704077572!2d-68.5906729379308!3d9.66234923217245!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sve!4v1731250553755!5m2!1sen!2sve"
           width="90%"
           height="120"
           style={{ border: "0" }}
           loading="lazy"
-          title={"Ubicación"}
+          title={"Inversiones Boara"}
         ></iframe>
       </section>
     </footer>
