08163cb6fc15f55e4a5836da72e62add2ed4eac8
TeKronS <asper.tk93@gmail.com>
2025-02-07 18:42:11 -0400

    add categorys

--- src/components/MenuDesplegable/categories.ts
+++ src/components/MenuDesplegable/categories.ts
@@ -4,7 +4,13 @@ const categories = [
   { value: "san valentin", name: "San Valentín" },
   { value: "dia de la madre", name: "Día de la Madre" },
   { value: "dia del padre", name: "Día del Padre" },
-  { value: "otros", name: "Otros" },
+  { value: "globos y rosas", name: "Globos y Rosas" },
+  { value: "ramos de rosas", name: "Ramos de Rosas" },
+  { value: "bandeja de desayuno", name: "Bandeja de Desayuno" },
+  { value: "frutales", name: "Frutales" },
+  { value: "15 años", name: "15 Años" },
+  { value: "bouquets", name: "Bouquets" },
+  { value: "personalizados", name: "Personalizados" },
 ];
 
 export { categories };
